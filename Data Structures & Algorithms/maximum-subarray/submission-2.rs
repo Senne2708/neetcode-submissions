c7b4a98199f4eef6870e9c237bb240487d7ec569
Senne van Doorn <56650279+Senne2708@users.noreply.github.com>
2026-04-06 11:21:34 +0100

    Add: maximum-subarray - submission-2

---
+++ Data Structures & Algorithms/maximum-subarray/submission-2.rs
@@ -0,0 +1,19 @@
+use std::cmp;
+
+impl Solution {
+    pub fn max_sub_array(nums: Vec<i32>) -> i32 {
+        let mut maxSum = nums[0];
+        let mut curSum = 0; 
+
+        for n in nums.iter() {
+            // 1. curSum must not be negative
+            curSum = cmp::max(curSum, 0);
+            // 2. Add current number to current sum 
+            curSum += n;
+            // 3. Check if the current sum is greater than the max sum
+            maxSum = cmp::max(maxSum, curSum);
+        }
+
+        maxSum
+    }
+}
